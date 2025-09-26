3ed79b0e42b8a0c151cb68322d4087426437c3fa
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2025-09-26 15:00:52 -0500

    Add files via upload

--- WindowsCleanerUtility.bat
+++ WindowsCleanerUtility.bat
@@ -1,5 +1,5 @@
 @echo off
-mode con cols=122 lines=40
+mode con cols=129 lines=40
 chcp 65001
 cls
 
@@ -50,12 +50,12 @@ echo   [40;31m ╚╗╔╗╔╝╔╣╠╗║║ ║║║╔╝╚╝║║
 echo   [40;31m  ╚╝╚╝ ╚══╝╚╝ ╚═╝╚═══╝╚═══╝ ╚╝╚╝ ╚═══╝ [40;34m  ╚═══╝╚═══╝╚═══╝╚╝ ╚╝╚╝ ╚═╝╚═══╝╚╝╚═╝  [40;31m  ╚═══╝ ╚══╝ ╚══╝╚═══╝╚══╝ ╚══╝   ╚╝  
 echo.
 echo.
-echo                                          [40;33m ╔════════════════════════════════╗
-echo                                          [40;33m ║    Windows Cleaner Utility     ║                                              
-echo                                          [40;33m ║    coded by Chainski Tools     ║                
-echo                                          [40;33m ╚════════════════════════════════╝    
+echo                                               [40;33m ╔═════════════════════════════════╗
+echo                                               [40;33m ║     Windows Cleaner Utility     ║                                              
+echo                                               [40;33m ║        coded by Chainski        ║                
+echo                                               [40;33m ╚═════════════════════════════════╝    
 
-title Windows Cleaner Utility %ver% by Chainski Tools 
+title Windows Cleaner Utility %ver% by Chainski 
 echo.
 echo [40;32m Press a button from 1 to 5 - each of these buttons has its own function as described below:
 echo.
@@ -89,7 +89,7 @@ DISM /Online /Cleanup-Image /RestoreHealth
 cls
 echo Windows Repaired Successfully!
 echo --------------------------------------------------
-echo Press ENTER KEY yo Continue.
+echo Press ENTER KEY to Continue.
 echo --------------------------------------------------
 pause >nul
 goto menu
@@ -115,32 +115,41 @@ if %errorlevel%==2 exit
 :clear
 rem Delete Temporary Files
 
-RMDIR "%tmp%" /S /Q >nul 2>nul
-del /s /f /q %temp%\*.* >nul 2>nul
-cleanmgr.exe /autoclean :: Used to delete old files left after upgrading a Windows build
-del /s /f /q %windir%\temp\*.* >nul 2>nul
-cls
-
 echo [Cleaning] and Optimizations in progress...
 timeout /t 2 /nobreak >nul
 echo.
 
 echo [Cleaning] Temporary Files && color c
 timeout /t 2 /nobreak >nul
+cleanmgr /autoclean 
+dism /online /cleanup-image /analyzecomponentstore
+dism /online /cleanup-image /startcomponentcleanup
+del /s /q /f  %systemdrive%\$Recycle.bin\* >nul 2>nul
+del /f /q /s "%appdata%\Microsoft\Windows\PowerShell\PSReadLine\*.*" >nul 2>nul
+
+taskkill /f /im explorer.exe
+echo You will need to restart the PC to finish rebuilding your icon cache.
+DEL /F /S /Q /A %LocalAppData%\Microsoft\Windows\Explorer\thumbcache_*.db  >nul 2>nul
+DEL /f /s /q /a %LocalAppData%\Microsoft\Windows\Explorer\*.db  >nul 2>nul
+start explorer.exe
 echo.
 
-:: Empties the RecycleBin
-del /s /q /f  %systemdrive%\$Recycle.bin\* >nul 2>nul
+net stop wuauserv
+del /s /q "C:\Windows\SoftwareDistribution\Download\*" >nul 2>nul
+net start wuauserv
+echo.
 
+del /s /f /q "%temp%\*.*" >nul 2>nul
+del /f /s /q %systemroot%\memory.dmp  >nul 2>nul
+del /f /s /q %systemroot%\Minidump\*.*  >nul 2>nul
 del /s /f /q %windir%\temp\*.* >nul 2>nul
+del /s /f /q %LOCALAPPDATA%\CrashDumps\*.* >nul 2>nul
 del /s /f /q %windir%\Prefetch\*.* >nul 2>nul
 del /s /f /q %LOCALAPPDATA%\Microsoft\Windows\Caches\*.* >nul 2>nul
-del /s /f /q %windir%\SoftwareDistribution\Download\*.* >nul 2>nul
 del /s /f /q %programdata%\Microsoft\Windows\WER\Temp\*.* >nul 2>nul
 del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.* >nul 2>nul
 del /f /s /q %systemdrive%\*.tmp 2>nul >nul
-rd /s /f /q %windir%\history 2>nul >nul
-rd /s /f /q %windir%\cookies 2>nul >nul
+del /f /s /q "%userprofile%\cookies\*.*" >nul 2>nul
 
 
 echo [Cleaning] Log Files && color b 
@@ -148,31 +157,32 @@ timeout /t 2 /nobreak >nul
 echo.
 
 REM Delete Log Files
-del /f /s /q %systemdrive%\*.log >nul 2>nul
-del /f /s /q %systemdrive%\*.old >nul 2>nul
-del /f /s /q %systemdrive%\*.trace >nul 2>nul
-del /f /s /q %windir%\*.bak >nul 2>nul
-del /s /f /q %windir%\Logs\CBS\CbsPersist*.log >nul 2>nul
-del /s /f /q %windir%\Logs\MoSetup\*.log >nul 2>nul
-del /s /f /q %windir%\Panther\*.log >nul 2>nul
-del /s /f /q %windir%\logs\*.log >nul 2>nul
-del /s /f /q %localappdata%\Microsoft\Windows\WebCache\*.log >nul 2>nul
-rd /s /f /q %localappdata%\Microsoft\Windows\INetCache\*.log >nul 2>nul
+del /f /s /q "%systemdrive%\*.log" >nul 2>nul
+del /f /s /q "%systemdrive%\*.old" >nul 2>nul
+del /f /s /q "%systemdrive%\*.trace" >nul 2>nul
+del /f /s /q "%windir%\*.bak" >nul 2>nul
+del /s /f /q "%windir%\Logs\CBS\CbsPersist\*.log" >nul 2>nul
+del /s /f /q "%windir%\Logs\MoSetup\*.log" >nul 2>nul
+del /s /f /q "%windir%\Panther\*.log" >nul 2>nul
+del /s /f /q "%windir%\Logs\*.log" >nul 2>nul
+del /s /f /q "%localappdata%\Microsoft\Windows\WebCache\*.log" >nul 2>nul
+del /s /f /q "%localappdata%\Microsoft\Windows\INetCache\*.log" >nul 2>nul
 
-
-:: Remove Event Logs.
-wevtutil.exe cl Application
-wevtutil.exe cl System
+:: Clear event logs
+echo [Cleaning] Event Logs...
+echo.
+for /f "tokens=*" %%L in ('wevtutil el') do (
+    wevtutil cl "%%L" >nul 2>nul
+)
 
 echo [Cleaning] Remnant Driver Files && color 9
 timeout /t 2 /nobreak >nul
 echo.
 
-rem Delete Remnant Driver Files (Not needed because already installed)
+
 del /s /f /q %SYSTEMDRIVE%\AMD\*.* >nul 2>nul
 del /s /f /q %SYSTEMDRIVE%\NVIDIA\*.* >nul 2>nul
 del /s /f /q %SYSTEMDRIVE%\INTEL\*.* >nul 2>nul
-
 
 echo [Cleaning] Windows Defender Cache/Logs && color 3
 timeout /t 2 /nobreak >nul
@@ -204,7 +214,8 @@ echo [Enabling] Ultimate Performance Mode && color b
 timeout /t 2 /nobreak >nul
 echo.
 
-echo Successfully Enabled Ultimate Performance Mode ! && color c
+color a
+echo Successfully Enabled Ultimate Performance Mode ! 
 timeout /t 2 /nobreak >nul
 echo.
 
@@ -213,6 +224,7 @@ powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 95533644-e700-4a7
 powercfg -setactive 95533644-e700-4a79-a56c-a89e8cb109d9 >nul 2>nul
 
 :clear-error
+color c
 echo Unfortunately, we were unable to remove all of the Temporary files (some are being used by other processes).
 echo You can manually open the Temporary Folders and try to delete the remaining files.
 echo.
@@ -233,7 +245,7 @@ goto menu
 
 :good-to-go
 cls
-echo Your PC doesn't need any cleaning. Your PC is good to go!
+echo Your PC doesn't need any cleaning try again later.
 pause
 goto menu
 
